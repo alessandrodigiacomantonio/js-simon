d0f3c733bfd2454e0fd89853683304044d9a10f6
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-21 16:26:00 +0100

    setTimeout impostato a 3 secondi per velocizzare i test

--- js/script.js
+++ js/script.js
@@ -6,6 +6,19 @@ $(document).ready(function() {
     } while (jQuery.inArray(random,randomNumbers)!= -1);
     randomNumbers[i] = random;
   }
-  console.log(randomNumbers);
-
+  alert('Benvenuto, lo scopo di questo giochino è testare la tua memoria.\n\nQuando ti sentirai pronto, schiaccia il tasto "ØK" per far scomparire i numeri sottostanti.\n\nDopo 30 secondi, ti verrà chiesto di riscriverli e guadagnerai 100 punti per ogni numero indovinato.\n\nIn bocca al lupo :D \n\n'+ randomNumbers.join(' - '));
+  setTimeout( function() {
+    var userPromptNumbers = [];
+    var number;
+    var point = 0;
+    for (var i = 0; i < 5; i++) {
+      do {
+        number = parseInt(prompt('inserisci il '+(i+1)+ '° numero'));
+      } while (jQuery.inArray(number, userPromptNumbers)!= -1 || number < 1 || number > 100 || isNaN(number));
+      userPromptNumbers[i] = number;
+      if (jQuery.inArray(number, randomNumbers)!=-1)
+      point+=100;
+    }
+    alert('Numeri da ricordare: '+randomNumbers.join(' - ')+'\n\nNumeri inseriti: '+userPromptNumbers.join(' - ')+'\n\nPunti accumulati: '+point);
+  },3*1000);
 });
